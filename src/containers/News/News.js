e8a8709ec333e38a0e73e4012ae10c5c1309ea62
Kevin Krato <kratok08@gmail.com>
2019-05-30 15:49:01 -0600

    Invoke async fetch on mount

--- src/containers/News/News.js
+++ src/containers/News/News.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 import "./News.scss";
+import { connect } from "react-redux";
+import { fetchIEXnews } from "../../utils/api/apiCalls";
 
 class News extends Component {
+  state = {
+    news: []
+  };
+
+  componentDidMount() {
+    this.getNews();
+  }
+
+  getNews = async () => {
+    const news = await fetchIEXnews();
+    const { headline, image, summary, datetime, source, related } = news;
+    const timeStamp = new Date(datetime * 1000).toString();
+    const newsData = { headline, image, summary, timeStamp, source, related };
+    this.setState({ news: newsData });
+  };
   render() {
+    const { headline, summary, timeStamp, source, related } = this.state.news;
     return (
-      <section className="News">
-        <h3>News Component</h3>
-      </section>
+      <article className="News">
+        <section>
+          <header>
+            <h3>{headline}</h3>
+            <p>Stock Ticker: {related}</p>
+          </header>
+          <div class="body">
+            <p className="summary">{summary}</p>
+            <p className="date">{timeStamp}</p>
+            <p className="source">{source}</p>
+          </div>
+        </section>
+      </article>
     );
   }
 }
 
-export default News;
+const mapStateToProps = state => ({
+  newsReducers: state.newsReducers
+});
+
+export default connect(mapStateToProps)(News);
